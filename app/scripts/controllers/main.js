6a1c021216af533608ab483647fe4c264cf0ed52
Luis Serota <luis@serota.net>
2016-09-24 12:07:07 -0400

    Built basic framework

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,48 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name nbahackathonApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the nbahackathonApp
- */
 angular.module('nbahackathonApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+
+  	$scope.curQuarter = 1;
+  	$scope.curTime = "12:00"; 
+
+
+  	$scope.liveGames = [
+  		{
+	  		HomeT: "Los Angeles Lakers",
+	  		AwayT: "Houston Rockets"
+  		},
+  		{
+	  		HomeT: "Dallas Mavericks",
+	  		AwayT: "San Antonio Spurs"
+  		},
+  		{
+	  		HomeT: "Boston Celtics",
+	  		AwayT: "Brooklyn Nets"
+  		}
+  	];
+
+  	$scope.currentGame = $scope.liveGames[0]; // Default to first game in list
+  	$scope.selectedTeam = $scope.currentGame.HomeT; // Default to home team
+
+  	$scope.startquarter = function() {
+  		$scope.curQuarter = $scope.curQuarter + 1;
+  	};
+
+  	// Choose a game from sidebar
+  	$scope.chooseGame = function(index) {
+  		$scope.currentGame = $scope.liveGames[index];
+  		$scope.selectedTeam = $scope.currentGame.HomeT;
+  	};
+
+  	$scope.selectTeam = function(teamName) {
+  		$scope.selectedTeam = teamName;
+  	};
+
+  	$scope.getTeamName = function(fullName) {
+  		var n = fullName.split(" ");
+    	return n[n.length - 1];
+  	};
+
+
   });
