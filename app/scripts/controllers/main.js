fead0bbbe174c82abdc67cc70867cf8921487083
Luis Serota <luis@serota.net>
2016-09-24 09:41:26 -0400

    ng template

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nbahackathonApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nbahackathonApp
+ */
+angular.module('nbahackathonApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
